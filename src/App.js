bd5214b147c0deaadbde7c3aa9da038544191e12
Valeria Torres <torresvalemdz@gmail.com>
2020-03-18 16:34:54 -0300

    mi primer componente 3 distintas maneras

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// function Hello( props ){
+// return <h2>{props.titulo}</h2>
+// }
+
+// const Hello = (props) => <h2>{props.titulo}</h2>;
+
+class Hello extends Component{
+    render(){
+        return  <h2>{ this.props.titulo }</h2>; 
+    }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    return ( <div className = "App" >
+        <header className = "App-header" >
+        <img src = { logo }
+        className = "App-logo" 
+        alt="logo"  />
+        <Hello titulo='Hello from props y class' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        Edit < code > src / App.js </code> and save to reload yea baby .  
+		</p> 
+		<a className = "App-link"
+			href = "https://reactjs.org"
+			target = "_blank"
+			rel = "noopener noreferrer" >
+        Learn React </a>  
+		</header> 
+		</div>
+    );
 }
 
 export default App;
