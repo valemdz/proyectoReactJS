b763bfe50dcc69763c860e11119725fcb7e9b216
Valeria Torres <torresvalemdz@gmail.com>
2020-03-18 18:04:50 -0300

    todo sobre props

--- src/App.js
+++ src/App.js
@@ -14,22 +14,60 @@ class Hello extends Component{
     }
 }
 
+class TituloEspecial extends Component{
+    render(){
+    return <h5>{ this.props.texto }</h5>
+    }
+}
+
+TituloEspecial.defaultProps = {
+    texto:' Default texto en h5'
+}
+
+class Texto extends Component{
+    render(){
+
+        const {
+            arrayOfNmbers,
+            isActivated,
+            number,
+            multiply,
+            objectWithInfo,
+            texto,
+            titulo
+        } = this.props;
+
+        const textoSegunBoolean = isActivated? 'Verdadero!!!':'Falso';
+        const mappedNumbers = arrayOfNmbers.map( multiply );
+
+        return <div> 
+                {titulo}
+               <p>{ texto }</p>
+               <p>{ number }</p>
+               <p>{ textoSegunBoolean }</p> 
+               <p>{ mappedNumbers.join(', ') }</p>
+               <p>{ JSON.stringify( objectWithInfo ) } </p>
+               </div>
+    }
+}
+
 function App() {
     return ( <div className = "App" >
-        <header className = "App-header" >
-        <img src = { logo }
-        className = "App-logo" 
-        alt="logo"  />
-        <Hello titulo='Hello from props y class' />
-        <p>
-        Edit < code > src / App.js </code> and save to reload yea baby .  
-		</p> 
-		<a className = "App-link"
-			href = "https://reactjs.org"
-			target = "_blank"
-			rel = "noopener noreferrer" >
-        Learn React </a>  
-		</header> 
+            <header className = "App-header" >
+
+                
+                <TituloEspecial texto='ya tiene valor el h5' />
+
+                <Texto 
+                    arrayOfNmbers={[2,4,8]}
+                    isActivated
+                    number={2}
+                    multiply={ ( n ) => n*-1}
+                    objectWithInfo= {{nombre: 'Valeria', apellido: 'Torres', 'edad':41 }}
+                    texto='Deberias aprender ReactJS' 
+                    titulo={ <h1>Este es el titulo en h1 </h1>}		 />
+
+            </header> 
 		</div>
     );
 }
