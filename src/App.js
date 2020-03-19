b36b95214bc161302dcab659e75f59db345eb48d
Valeria Torres <torresvalemdz@gmail.com>
2020-03-19 17:37:34 -0300

    como utilizar listar y poner key unica

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import Condicional from './sections/conditional';
+//import Condicional from './sections/conditional';
 
 
-function App() {
-    return ( <div className = "App" >
-          <Condicional />
+
+class  App extends Component {
+
+  render(){
+
+     const numbers  = [1,1,3,4,5];
+
+     return ( <div className = "App" >               
+                        
+            <h4>Trabajando con listas</h4>
+            { numbers.map( ( nu, index ) =>  <p key={index} >Soy el numero { nu }</p>  ) }
+      
 		</div>
     );
+
+  }
 }
 
 export default App;
