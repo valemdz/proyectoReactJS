64933174bd1f0b6fccffdc0aa3b4d25d99771ded
Valeria Torres <torresvalemdz@gmail.com>
2020-03-20 11:50:03 -0300

    Manejo de Evento con handleEvent

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import Condicional from './sections/conditional';
 
 
-function App() {
-    return ( <div className = "App" >
+class  App extends Component {
 
-      <h4>Practica de Eventos</h4>
+    handleClick( e ) {
+      console.log( e );
+      console.log( e.nativeEvent );      
+      alert('Manejado con un handle');
+    }
 
-      <button onClick = { () => { return alert('Haz click aqui') } } > Haz click aqui </button>
-          
-		</div>
-    );
+    render(){
+
+      return ( <div className = "App" >
+
+        <h4>Practica de Eventos</h4>
+
+        <button onClick = { this.handleClick } > Haz click aqui </button>
+            
+      </div>
+      );
+    }  
 }
 
 export default App;
